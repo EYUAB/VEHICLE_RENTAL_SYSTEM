f60e0a2dd6a12863c9f8c7ef32ef0d642fbd0721
Eyob Yibabie <eyobyibabie01@gmail.com>
2023-03-31 09:40:02 +0300

    configuring with firebase

---
+++ lib/Components/logIn.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class LogIn extends StatefulWidget {
+  const LogIn({super.key});
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
