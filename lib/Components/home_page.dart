8d482af1f4a3a1b58f2de440a518fdcfb420cdb6
Eyob Yibabie <eyobyibabie01@gmail.com>
2023-03-31 10:57:19 +0300

    Refactoring the wudgets

---
+++ lib/Components/home_page.dart
@@ -0,0 +1,124 @@
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter/material.dart';
+import 'package:vehicle_rental_system/Components/carousel.dart';
+import 'package:vehicle_rental_system/Components/horizontalScrol.dart';
+import 'package:vehicle_rental_system/Components/vehicle_lists.dart';
+
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text('Vehicle Rental System'),
+          actions: [
+            IconButton(onPressed: null, icon: Icon(Icons.search,color: Colors.white,),),
+          ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              UserAccountsDrawerHeader(accountName: Text('Jermi'), accountEmail: Text('Jermi01@gmail.com'),
+              currentAccountPicture: GestureDetector(child: 
+              CircleAvatar(
+                child: Icon(Icons.person),
+              ),
+              ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Home Page'),
+                ),
+              ),
+               InkWell(
+                onTap: () {},
+                child: ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Home Page'),
+                ),
+              ),
+               InkWell(
+                onTap: () {},
+                child: ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Home Page'),
+                ),
+              ),
+               InkWell(
+                onTap: () {},
+                child: ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Home Page'),
+                ),
+              ),
+               InkWell(
+                onTap: () {},
+                child: ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Home Page'),
+                ),
+              ),
+              Divider(
+                thickness: 0.5,
+              ),
+              InkWell(
+                onTap: () {},
+                child: ListTile(
+                leading: Icon(Icons.settings,color: Colors.blue,),
+                title: Text('setting'),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: ListTile(
+                leading: Icon(Icons.help,color: Colors.amber,),
+                title: Text('about'),
+                ),
+              ),
+              
+            ],
+          ),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              // horizontal slider
+              CarouselWidget(),
+              const Padding(padding: EdgeInsets.all(8),
+              child: Text('Catagory'),
+              ),
+              Padding(padding: EdgeInsets.all(8),
+              // horizontal scroll view 
+              child: HorizontalScroll(),
+              ),
+              const Padding(padding: EdgeInsets.all(8),
+              child: Text('Available Vehicles'),
+              ),
+                 Container(
+                  height: 400,
+                   child: Padding(padding: EdgeInsets.all(8),
+                               child: AvailableVehicleLists(),
+                               ),
+                 ),
+            ],
+          ),
+        ),
+        ),
+    );
+  }
+}
--- lib/Components/logIn.dart
+++ lib/Components/logIn.dart
@@ -1,3 +1,5 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -11,6 +13,38 @@ class LogIn extends StatefulWidget {
 class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+         appBar: AppBar(
+            backgroundColor: Colors.red,
+            title: Text('Vehicle Rental System'),
+            actions: [
+              IconButton(onPressed: null, icon: Icon(Icons.search,color: Colors.white,),),
+            ],
+          ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24,vertical: 50),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 60,
+                child: Image.asset('Images/car_logo.png'),),
+                SizedBox(height: 20,),
+              TypewriterAnimatedTextKit(
+              text: [
+                'Vehicle Rental'
+              ],
+              speed: Duration(milliseconds: 200),
+              textStyle: TextStyle( fontSize: 45.0,
+              color: Colors.black,
+              fontWeight: FontWeight.w900,),
+                 ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
