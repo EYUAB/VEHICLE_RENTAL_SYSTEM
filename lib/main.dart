465d94a512d48cf2fa920789a8e56cf814abb814
Eyob Yibabie <eyobyibabie01@gmail.com>
2023-03-31 16:15:18 +0300

    creating splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 
 import 'package:flutter/material.dart';
-import 'package:vehicle_rental_system/Components/logIn.dart';
+import 'package:vehicle_rental_system/Components/splash_screen.dart';
 
 void main(List<String> args) {
    WidgetsFlutterBinding.ensureInitialized();
   runApp( MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: LogIn(),
+    home: SplashScreenWidget(),
   ),
   );
 }
