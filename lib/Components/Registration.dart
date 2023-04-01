3761ed8d0bad27e3875a7ea8151ea182340fac5f
Eyob Yibabie <eyobyibabie01@gmail.com>
2023-04-01 11:42:47 +0300

    adding validation for textFields

--- lib/Components/Registration.dart
+++ lib/Components/Registration.dart
@@ -16,90 +16,131 @@ class RegistrationPage extends StatefulWidget {
 }
 
 class _RegistrationPageState extends State<RegistrationPage> {
+    final _formKey=GlobalKey<FormState>();
+ 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                height: 60,
-                child: Hero(
-                  tag: Text('hero'),
-                  child: Image.asset('Images/car_logo.png')),),
-                SizedBox(height: 20,),
-              TypewriterAnimatedTextKit(
-              text: [
-                'Vehicle Rental'
-              ],
-              speed: Duration(milliseconds: 200),
-              textStyle: TextStyle( fontSize: 45.0,
-              color: Colors.black,
-              fontWeight: FontWeight.w900,),
-                 ),
-                  SizedBox(
-                height: 48.0,
-              ),
-              TextField(
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.name,
-                onChanged: (value) {
-                  //Do something with the user input.
-                  
-                },
-                decoration: Constants.kInputDecoration.copyWith(hintText: 'Enter your full_name'),
-              ),
-              SizedBox(height: 8,),
-              TextField(
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  //Do something with the user input.
-                  
-                },
-                decoration: Constants.kInputDecoration.copyWith(hintText: 'Enter your phone number'),
-              ),
-              SizedBox(height: 8,),
-               TextField(
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                  //Do something with the user input.
-                  
-                },
-                decoration: Constants.kInputDecoration.copyWith(hintText: 'Enter your email'),
-              ),
-              SizedBox(height: 8,),
-                TextField(
-                textAlign: TextAlign.center,
-                obscureText: true,
-                onChanged: (value) {
-                  //Do something with the user input.
-                  
-                },
-                decoration: Constants.kInputDecoration.copyWith(hintText: 'Enter your password'),
-              ),
-              SizedBox(height: 24,),
-              Buttons(buttonType: 'Register', onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
-              }, color: Colors.blueAccent),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text('Do you have an account?',style: TextStyle(color: Colors.black45,fontSize: 15),),
-                  MaterialButton(onPressed: (){},
-                  child: InkWell(
-                    onTap: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>LogIn()));
-                    },
-                    child: Text('Sign In',style: TextStyle(color: Colors.blueAccent,fontSize: 15),)),)
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  height: 60,
+                  child: Hero(
+                    tag: Text('hero'),
+                    child: Image.asset('Images/car_logo.png')),),
+                  SizedBox(height: 20,),
+                TypewriterAnimatedTextKit(
+                text: [
+                  'Vehicle Rental'
                 ],
-              ),
-            ],
+                speed: Duration(milliseconds: 200),
+                textStyle: TextStyle( fontSize: 45.0,
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.w900,),
+                   ),
+                    SizedBox(
+                  height: 48.0,
+                ),
+                TextFormField(
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.name,
+                  onChanged: (value) {
+                    //Do something with the user input.
+                    
+                  },
+                  decoration: Constants.kInputDecoration.copyWith(hintText: 'Enter your full_name'),
+                   validator: (value){
+                    if(value!.isEmpty || !RegExp(r'^[A-Z a-z]+$').hasMatch(value!)) {
+                      return 'Enter correct name';
+
+                    }
+                    else return null;
+                  },
+                ),
+                SizedBox(height: 8,),
+                TextFormField(
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    //Do something with the user input.
+                    
+                  },
+                  decoration: Constants.kInputDecoration.copyWith(hintText: 'Enter your phone number'),
+                   validator: (value){
+                    if(value!.isEmpty || !RegExp(r'^[+]*[0-9]+$').hasMatch(value!)) {
+                      return 'Enter correct phone number';
+
+                    }
+                    else return null;
+                  },
+                ),
+                SizedBox(height: 8,),
+                 TextFormField(
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) {
+                    //Do something with the user input.
+                    
+                  },
+                  decoration: Constants.kInputDecoration.copyWith(hintText: 'Enter your email'),
+                   validator: (value){
+    if(value!.isEmpty || !RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$').hasMatch(value!)) {
+                      return 'Enter correct email';
+
+                    }
+                    else return null;
+                  },
+                ),
+                SizedBox(height: 8,),
+                  TextFormField(
+                  textAlign: TextAlign.center,
+                  obscureText: true,
+                  onChanged: (value) {
+                    //Do something with the user input.
+                    
+                  },
+                  decoration: Constants.kInputDecoration.copyWith(hintText: 'Enter your password'),
+                   validator: (value){
+                    if(value!.isEmpty) {
+                      return 'Enter correct password';
+
+                    }
+                    if(value.length < 6 && value.length >0){
+                      return 'the password must greater than 5';
+                    }
+                    else return null;
+                  },
+                ),
+                SizedBox(height: 24,),
+                Buttons(buttonType: 'Register', onPressed: (){
+                    if(_formKey.currentState!.validate()){
+                    final snackBar=SnackBar(content: Text('Submitting form'));
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));  
+                  }
+                }, color: Colors.blueAccent),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text('Do you have an account?',style: TextStyle(color: Colors.black45,fontSize: 15),),
+                    MaterialButton(onPressed: (){},
+                    child: InkWell(
+                      onTap: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>LogIn()));
+                      },
+                      child: Text('Sign In',style: TextStyle(color: Colors.blueAccent,fontSize: 15),)),)
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
     );
--- lib/Components/logIn.dart
+++ lib/Components/logIn.dart
@@ -13,109 +13,135 @@ class LogIn extends StatefulWidget {
 }
 
 class _LogInState extends State<LogIn> {
+  final _formKey=GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                height: 60,
-                child: Hero(
-                  tag: Text('hero'),
-                  child: Image.asset('Images/car_logo.png')),),
-                SizedBox(height: 20,),
-              TypewriterAnimatedTextKit(
-              text: [
-                'Vehicle Rental'
-              ],
-              speed: Duration(milliseconds: 200),
-              textStyle: TextStyle( fontSize: 45.0,
-              color: Colors.black,
-              fontWeight: FontWeight.w900,),
-                 ),
-                  SizedBox(
-                height: 48.0,
-              ),
-               TextField(
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                  //Do something with the user input.
-                  
-                },
-                decoration: Constants.kInputDecoration.copyWith(hintText: 'Enter your email'),
-              ),
-              SizedBox(height: 15,),
-                TextField(
-                textAlign: TextAlign.center,
-                obscureText: true,
-                onChanged: (value) {
-                  //Do something with the user input.
-                  
-                },
-                decoration: Constants.kInputDecoration.copyWith(hintText: 'Enter your password'),
-              ),
-              SizedBox(height: 24,),
-              Buttons(buttonType: 'Log in', onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
-              }, color: Colors.lightBlueAccent),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text('Don\'t have account?',style: TextStyle(color: Colors.black45,fontSize: 15),),
-                  
-                  
-                  MaterialButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>RegistrationPage()));
-                  },
-                  child: Text('Create New Account',style: TextStyle(color: Colors.lightBlueAccent,fontSize: 15),),)
-                ],
-              ),
-              SizedBox(height: 25,),
-              Row(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Expanded(
-                  child: Divider(
-                    color: Colors.black45,
-                  ),
+                Container(
+                  height: 60,
+                  child: Hero(
+                    tag: Text('hero'),
+                    child: Image.asset('Images/car_logo.png')),),
+                  SizedBox(height: 20,),
+                TypewriterAnimatedTextKit(
+                text: [
+                  'Vehicle Rental'
+                ],
+                speed: Duration(milliseconds: 200),
+                textStyle: TextStyle( fontSize: 45.0,
+                color: Colors.lightBlueAccent,
+                fontWeight: FontWeight.w900,),
+                   ),
+                    SizedBox(
+                  height: 48.0,
                 ),
-                Text('Or sign in with google',style: TextStyle(color: Colors.black45,fontSize: 15),),
-                Expanded(
-                  child: Divider(
-                    color: Colors.black45,
+                 TextFormField(
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) {
+                    //Do something with the user input.
+                    
+                  },
+                  validator: (value){
+                    if(value!.isEmpty || !RegExp(r'^[A-Z a-z]+$').hasMatch(value!)) {
+                      return 'Enter correct name';
+
+                    }
+                    else return null;
+                  },
+                  decoration: Constants.kInputDecoration.copyWith(hintText: 'Enter your email'),
+                ),
+                SizedBox(height: 15,),
+                  TextFormField(
+                  textAlign: TextAlign.center,
+                  obscureText: true,
+                  onChanged: (value) {
+                    //Do something with the user input.
+                    
+                  },
+                   validator: (value){
+                    if(value!.isEmpty) {
+                      return 'Enter correct password';
+
+                    }
+                    if(value.length<6 && value.length>0){
+                      return 'the password must be greater 5';
+                    }
+                    else return null;
+                  },
+                  decoration: Constants.kInputDecoration.copyWith(hintText: 'Enter your password'),
+                ),
+                SizedBox(height: 24,),
+                Buttons(buttonType: 'Log in', onPressed: (){
+                  if(_formKey.currentState!.validate()){
+                    final snackBar=SnackBar(content: Text('Authenticating'));
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));  
+                  }               
+                }, color: Colors.lightBlueAccent),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text('Don\'t have account?',style: TextStyle(color: Colors.black45,fontSize: 15),),
+                    
+                    
+                    MaterialButton(onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>RegistrationPage()));
+                    },
+                    child: Text('Create New Account',style: TextStyle(color: Colors.lightBlueAccent,fontSize: 15),),)
+                  ],
+                ),
+                SizedBox(height: 25,),
+                Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: Colors.black45,
+                    ),
                   ),
+                  Text('Or sign in with google',style: TextStyle(color: Colors.black45,fontSize: 15),),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.black45,
+                    ),
+                  ),
+                ],
+                ),
+                SizedBox(height: 30,),
+              
+                InkWell(
+                  onTap: (){},
+                  child: Container(
+                    decoration: BoxDecoration(
+                       borderRadius: BorderRadius.circular(32),
+                       border: Border.all(
+                        color: Colors.lightBlueAccent,
+                       )
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                           Padding(
+                            padding: EdgeInsets.only(right: 8),
+                            child: Image.asset('Images/google.png',width: 40,height: 40,)),
+                           Text('sign in with google',style: TextStyle(color: Colors.black45,fontSize: 18),),
+                           
+                     ],
+                     ),
+                    ),
                 ),
               ],
-              ),
-              SizedBox(height: 30,),
-    
-              InkWell(
-                onTap: (){},
-                child: Container(
-                  decoration: BoxDecoration(
-                     borderRadius: BorderRadius.circular(32),
-                     border: Border.all(
-                      color: Colors.lightBlueAccent,
-                     )
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                         Padding(
-                          padding: EdgeInsets.only(right: 8),
-                          child: Image.asset('Images/google.png',width: 40,height: 40,)),
-                         Text('sign in with google',style: TextStyle(color: Colors.black45,fontSize: 18),),
-                         
-                   ],
-                   ),
-                  ),
-              ),
-            ],
+            ),
           ),
         ),
     );
